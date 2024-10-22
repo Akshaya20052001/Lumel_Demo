c7d104eede1e775fe824f1d7f8c11f316d9e85ca
Akshaya20052001 <akshayapandiyan02@gmail.com>
2024-10-22 10:37:59 +0530

    Akshaya Update

---
+++ Best_Seller/src/org/Selenium/loop_Concepts.java
@@ -0,0 +1,19 @@
+package org.program;
+
+public class loop_Concepts {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i%2 != 0) {
+				System.out.println("Even numbers are "+ i);
+			}
+
+//			else {
+//				System.out.println("Old numbers are "+ i);
+//			}
+
+		}
+
+	}
+
+}
